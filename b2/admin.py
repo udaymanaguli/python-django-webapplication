3091275f12640cb6ae723aceef4df6e78c213162
udaymanaguli <59868945+udaymanaguli@users.noreply.github.com>
2025-07-24 19:13:50 +0530

    Delete b2 directory

--- b2/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Voter, Vote
-
-# Register your models here.
-admin.site.register(Voter)
-admin.site.register(Vote)
--- b2/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Choice
-
-
-class saved(forms.ModelForm):
-    class Meta:
-        model = Choice
-        fields = ["cidentity", "choice"]
