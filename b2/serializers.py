2f0921b13046149e223661644a78fe361a614a6c
udaymanaguli <59868945+udaymanaguli@users.noreply.github.com>
2026-01-31 22:29:25 -0500

    Update serializers.py

--- b2/serializers.py
+++ b2/serializers.py
@@ -1,1 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Vote
+
+class VoteRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['identity', 'dob', 'phone']  # Adjust fields to match your Vote model
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        return user
 
